c650fd3b75e3104fb71ba58eab1075cd1627b645
Jonathan Oswald <70522829+StreetLampLeMoose@users.noreply.github.com>
2023-07-15 14:50:00 -0600

    Update main.py
    added comment and put in default input.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from arithmetic_arranger import arithmetic_arranger1
 linelist =[]
-lineList = arithmetic_arranger1(['98 + 3g5', '3801 - 2', '45 + 43', '123 + 49'])
-print(" ".join(lineList[0]))
+lineList = arithmetic_arranger1(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]) 
+print(" ".join(lineList[0])) #these print statements join and print the elements of line list so they are correctly formatted. 
 print(" ".join(lineList[1]))
 print(" ".join(lineList[2]))
 print(" ".join(lineList[3]))
